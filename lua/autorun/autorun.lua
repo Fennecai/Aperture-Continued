139fc67018f02257bca6433284e576556e686368
Fennecai <1995aek@gmail.com>
2019-05-14 18:21:01 -0600

    removed experimental mapfixer code

--- lua/autorun/autorun.lua
+++ lua/autorun/autorun.lua
@@ -2,8 +2,6 @@
 AddCSLuaFile("aperture/main.lua")
 -- shared includes
 include("aperture/main.lua")
---Map fixer includes
-include("mapfixer/mapfixer_autorun.lua")
 if SERVER then
 	AddCSLuaFile()
 --resource.AddWorkshop( "862644776" ) -- Workshop download
